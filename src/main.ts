4a1350fbdd96b5a98f44c353bb17c98096cb26a3
相甫 <xiangfu_git@163.com>
2025-05-30 17:06:50 +0800

    feat:添加pinia

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,13 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 import router from "./router";
+import { store } from "./store";
 
 const app = createApp(App);
+
+// 路由
 app.use(router);
+// store
+app.use(store);
+
 app.mount("#app");
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,6 +1,12 @@
-<script setup lang="ts"></script>
-
 <template>
   <div>home</div>
   <RouterLink to="/tools">Go to Tools</RouterLink>
+  <div>{{ demoStore.count }}</div>
+  <div>{{ demoStore.doubleCount }}</div>
+  <button @click="demoStore.increment()">增加</button>
 </template>
+
+<script setup lang="ts">
+import { useDemoStore } from "@/store/modules/demo";
+const demoStore = useDemoStore();
+</script>
