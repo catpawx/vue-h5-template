74e1faf138efd7d5b2dea120104275b213378c75
相甫 <xiangfu_git@163.com>
2025-06-02 13:57:37 +0800

    feat:添加vant UI框架和自动导入方法unplugin-auto-import/vite，自动导入组件unplugin-vue-components/vite等插件

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -4,9 +4,25 @@
   <div>{{ demoStore.count }}</div>
   <div>{{ demoStore.doubleCount }}</div>
   <button @click="demoStore.increment()">增加</button>
+  <br />
+  <van-button type="primary" @click="showToastStub">
+    show vant toast
+  </van-button>
 </template>
 
 <script setup lang="ts">
 import { useDemoStore } from "@/store/modules/demo";
+import { onActivated, onMounted } from "vue";
 const demoStore = useDemoStore();
+
+onMounted(() => {
+  console.log("🚀🚀🚀======>>>onMounted");
+});
+onActivated(() => {
+  console.log("🚀🚀🚀======>>>onActivated");
+});
+
+const showToastStub = () => {
+  showToast("No need to import showToast");
+};
 </script>
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,33 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { VantResolver } from "@vant/auto-import-resolver";
 
 // https://vite.dev/config/
 
 export default defineConfig(({ mode }) => {
   return {
-    plugins: [vue()],
+    plugins: [
+      vue(),
+      AutoImport({
+        dts: "src/types/auto-imports.d.ts",
+        resolvers: [VantResolver()],
+      }),
+      Components({
+        dts: "src/types/components.d.ts",
+        resolvers: [VantResolver()],
+      }),
+    ],
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "./src"),
       },
+    },
+    server: {
+      host: true,
+      port: 10086,
     },
   };
 });
